3abbf25751a35278e50f13a8c28e22441206d6fb
braisf03 <114221451+braisf03@users.noreply.github.com>
2023-09-26 14:06:29 +0200

    Add files via upload

--- Prácticas2324/p_02/def2.ml
+++ Prácticas2324/p_02/def2.ml
@@ -6,11 +6,10 @@ let absf = function x -> if x > 0. then x else (-1.) *. x
 
 let even = function x-> x mod 2 = 0
 
-let rec next3 = function x -> if x mod 3 = 0 then x else next3 ( x + 1 )
+let next3 = function x -> if x mod 3 = 0 then x else x - x mod 3 + 3
 
 let is_a_letter = function x -> 
 	if 65 <= int_of_char x && int_of_char x <= 90 then true 
-	else if 97 <= int_of_char x && int_of_char x <= 122 then true 
-	else false
+	else 97 <= int_of_char x && int_of_char x <= 122
 	
 let string_of_bool = function x -> if x then "verdadero" else "falso"
